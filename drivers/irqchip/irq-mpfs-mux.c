56579001b92f73c6aca11c6a2010538087637867
Conor Dooley <conor.dooley@microchip.com>
2023-04-28 14:38:27 +0100

    mux logic fixes
    Signed-off-by: Conor Dooley <conor.dooley@microchip.com>

--- drivers/irqchip/irq-mpfs-mux.c
+++ drivers/irqchip/irq-mpfs-mux.c
@@ -3,20 +3,9 @@
  * Microchip PolarFire SoC (MPFS) GPIO IRQ MUX
  *
  * Author: Conor Dooley <conor.dooley@microchip.com>
- *
- * MUX Configuration:
- * - there are 3 gpio controllers on this SoC, each with 32 gpios
- * - there are significantly fewer than 96 interrupts allocated for gpio
- * - there are 3 muxed, or "non-direct" interrupts
- * - 14 are either gpio 0 or gpio 2's "direct" connections to the plic
- * - 24 are either gpio 1 or gpio 2's <ditto>
- * - the GPIO_INTERRUPT_FAB_CR register determines which of gpio m or 2 is wired
- *   directly, and which is non-direct
- * - the register is 32-bit but the mux has 38 lines. God only knows how those
- *   are meant to be configured. I claim that they are all gpio 1.
- * - anything not wired directly is muxed into the corresponding non-direct
- *   interrupt
  */
+
+#define pr_fmt(fmt) "mpfs-irq-mux: " fmt
 
 #include <linux/bits.h>
 #include <linux/interrupt.h>
@@ -29,13 +18,39 @@
 #include <linux/platform_device.h>
 
 #define MPFS_MUX_NUM_IRQS 41
-#define MPFS_MUX_GPIO0_2_MASK GENMASK(13, 0)
-#define MPFS_MUX_GPIO1_2_MASK GENMASK(31, 14)
+#define MPFS_MUX_NUM_DIRECT_IRQS 38
+#define MPFS_IRQS_PER_GPIO 32
 
-static const u32 mpfs_irq_mux_masks[3] = {
-	MPFS_MUX_GPIO0_2_MASK,
-	MPFS_MUX_GPIO1_2_MASK,
-	GENMASK(31, 0),
+/*
+ * There are 3 GPIO controllers on this SoC, each of which has 32 GPIOs.
+ * All GPIOs are capable of generating interrupts, for a total of 96.
+ * There are only 41 IRQs available however, so a configurable mux is used to
+ * ensure all GPIOs can be used for interrupt generation.
+ * 38 of the 41 interrupts are in what the documentation calls "direct mode",
+ * as they provide an exclusive connection from a GPIO to the PLIC.
+ * The 3 remaining interrupts are used to mux the interrupts which do not have
+ * a exclusive connection, one for each GPIO controller.
+ * A register is used to set this configuration of this mux, depending on how
+ * the "MSS Configurator" (FPGA configuration tool) has set things up.
+ * This is done by the platforms firmware, so access from Linux is read-only.
+ *
+ * Documentation also refers to GPIO controller 0 & 1 as "pad" GPIO controllers
+ * and GPIO controller 2 as the "fabric" GPIO controller. Despite that wording,
+ * all 3 are "hard" peripherals. If a bit is set in the mux's control register,
+ * the "pad" controller's interrupt will be put in "non-direct" mode. If
+ * cleared, the "fabric" controller's.
+ *
+ * Bits 0 to 13 mux between GPIO controller 1's first 14 GPIOs and GPIO
+ * controller 2's first 14. The remaining bits mux between the first 18 GPIOs
+ * of controller 1 and the last 18 GPIOS of controller 2.
+ *
+ * That leaves 6 exclusive, or "direct", interrupts remaining. These are,
+ * according to documentation, used by GPIO controller 1's lines 19 to 24.
+ */
+static const unsigned long mpfs_irq_mux_masks[3] = {
+	GENMASK_ULL(13, 0),
+	GENMASK_ULL(37, 14),
+	GENMASK_ULL(31, 0),
 };
 
 struct mpfs_irq_mux_irqchip {
@@ -53,33 +68,51 @@ struct mpfs_irq_mux {
 };
 
 static struct irq_chip mpfs_irq_mux_irq_chip = {
-	.name = "MPFS irq mux",
+	.name = "MPFS GPIO Interrupt Mux",
 };
 
 static inline unsigned long mpfs_irq_mux_get_muxxed_irqs(struct mpfs_irq_mux *priv, int i)
 {
-	u32 mux_config = priv->mux_config;
+	unsigned long mux_config = priv->mux_config;
+	unsigned long mask = priv->irqchip_data[i].reg_mask;
+	unsigned long muxxed_irqs;
 
 	/*
-	 * Perhaps a hack, but we know that only gpio2 has all the bits set in
-	 * its mask & from the register description:
-	 * Setting these bits will disable the Pad interrupt, and enable
-	 * the fabric GPIO interrupt for bits 31:0. When the bit is set the Pad
-	 * interrupt will be ORED into the GPIO0 & GPIO1 non-direct interrupts.
-	 * When the bit is not set the Fabric interrupt is ORED into the GPIO2
-	 * non-direct interrupt. To prevent ORING then the interrupt should not
-	 * be enabled in the GPIO block
+	 * Assume that all interrupts outside of the mask are in "direct" mode
+	 * to begin with. Anything not in the mask cannot be set to
+	 * "direct" mode, so it is a useful analogue.
+	 *
+	 * Handily, this will account for the 6 extra "direct" interrupts that
+	 * are connected to GPIO controller 1, which will be cleared by the
+	 * inverted mask.
 	 */
-	if (priv->irqchip_data[i].reg_mask == GENMASK(31, 0)) //this is really sloppy, remove the hack
+	muxxed_irqs = ~mask;
+
+	/*
+	 * If a bit is set in the mux, GPIO controller 2 is direct and
+	 * controllers 0 & 1 are muxxed.
+	 * Invert the bits in the configuration register, so that set bits
+	 * equate to non-direct mode, for GPIO controller 2.
+	 */
+	if (mask == mpfs_irq_mux_masks[2])
 		mux_config = ~mux_config;
 
-	return priv->irqchip_data[i].reg_mask & mux_config;
+	mux_config &= mask;
+	muxxed_irqs |= mux_config;
+
+	/*
+	 * For GPIO controller 1, the first interrupt is not aligned with the
+	 * start of register, rather with the start of the mask.
+	 */
+	muxxed_irqs >>= ffs(mask);
+
+	return muxxed_irqs;
 }
 
 static void mpfs_irq_mux_irq_handler(struct irq_desc *desc)
 {
 	struct mpfs_irq_mux_irqchip *irqchip_data = irq_desc_get_handler_data(desc);
-	int i = irqchip_data->i; //TODO maybe a hack?
+	int i = irqchip_data->i;
 	struct mpfs_irq_mux *priv = container_of(irqchip_data, struct mpfs_irq_mux,
 						irqchip_data[i]);
 	unsigned long muxxed_irqs;
@@ -89,8 +122,8 @@ static void mpfs_irq_mux_irq_handler(struct irq_desc *desc)
 
 	muxxed_irqs = mpfs_irq_mux_get_muxxed_irqs(priv, i);
 
-	for_each_set_bit(pos, &muxxed_irqs, 32)
-		generic_handle_domain_irq(irqchip_data->domain, pos);
+	for_each_set_bit(pos, &muxxed_irqs, MPFS_IRQS_PER_GPIO)
+		generic_handle_irq(irq_find_mapping(irqchip_data->domain, pos));
 
 	chained_irq_exit(irq_desc_get_chip(desc), desc);
 }
@@ -101,8 +134,9 @@ static int mpfs_irq_mux_irq_map(struct irq_domain *h, unsigned int irq,
 	struct mpfs_irq_mux_irqchip *irqchip_data = h->host_data;
 
 	irq_set_chip_data(irq, irqchip_data);
-	irq_set_chip_and_handler(irq, &mpfs_irq_mux_irq_chip, handle_level_irq); //may be wrong (handle_level_irq)
-	pr_info("mpfs-irq-mux: mapped %lu as %u\n", hwirq, irq);
+	irq_set_chip_and_handler(irq, &mpfs_irq_mux_irq_chip, handle_level_irq);
+
+	pr_info("mapped %lu as %u\n", hwirq, irq);
 
 	return 0;
 }
@@ -112,7 +146,7 @@ static int mpfs_irq_mux_irq_select(struct irq_domain *d, struct irq_fwspec *fwsp
 {
 	struct mpfs_irq_mux_irqchip *irqchip_data = d->host_data;
 	bool is_it_us = false;
-	int i = irqchip_data->i; //TODO maybe a hack?
+	int i = irqchip_data->i;
 	struct mpfs_irq_mux *priv = container_of(irqchip_data, struct mpfs_irq_mux,
 						irqchip_data[i]);
 
@@ -120,20 +154,20 @@ static int mpfs_irq_mux_irq_select(struct irq_domain *d, struct irq_fwspec *fwsp
 		return false;
 	}
 
-
-	// really this should be 38 + start (I think this comment assumes that
+	// really this should be MPFS_MUX_NUM_DIRECT_IRQS + start (I think this comment assumes that
 	// direct mode would mean adding more hwirqs)
-	u32 start = (irqchip_data->i - 38) * 32;
-	u32 end = start + 32;
+	u32 start = (irqchip_data->i - MPFS_MUX_NUM_DIRECT_IRQS) * MPFS_IRQS_PER_GPIO;
+	u32 end = start + MPFS_IRQS_PER_GPIO;
 
 	if (fwspec->param[0] < end && fwspec->param[0] >= start) {
 		u32 muxxed_irqs = mpfs_irq_mux_get_muxxed_irqs(priv, i);
+		pr_debug("muxxed_irqs: %x\n", muxxed_irqs);
 
 		if(muxxed_irqs & fwspec->param[0])
 			is_it_us = true;
 	}
 
-	pr_info("mpfs-irq-mux: is it us? %u, %pa %u\n", is_it_us, d, irqchip_data->i);
+	pr_info("is it us? %u, %pa %u\n", is_it_us, d, irqchip_data->i);
 	return is_it_us;
 }
 
@@ -145,7 +179,7 @@ static int mpfs_irq_mux_irq_xlate(struct irq_domain *d, struct device_node *node
 	*out_hwirq = intspec[0] - irqchip_data->offset;
 	*out_type = IRQ_TYPE_LEVEL_HIGH;
 
-	pr_info("mpfs-irq-mux: translated %u to %lu", intspec[0], *out_hwirq);
+	pr_info("translated %u to %lu", intspec[0], *out_hwirq);
 
 	return 0;
 };
@@ -172,7 +206,7 @@ static int __init mpfs_irq_mux_init(struct device_node *node, struct device_node
 
 	priv->mux_config = readl(priv->reg);
 
-	for (i = 0; i < (MPFS_MUX_NUM_IRQS - 3); i++)
+	for (i = 0; i < (MPFS_MUX_NUM_DIRECT_IRQS); i++)
 		;
 
 	/*
@@ -185,23 +219,23 @@ static int __init mpfs_irq_mux_init(struct device_node *node, struct device_node
 		if (priv->irqchip_data[i].irq <= 0)
 			return -1; //TODO: add teardown code
 
-		domain = irq_domain_add_linear(node, 32, &mpfs_irq_mux_domain_ops,
+		domain = irq_domain_add_linear(node, MPFS_IRQS_PER_GPIO, &mpfs_irq_mux_domain_ops,
 					       &priv->irqchip_data[i]);
 		if (!domain)
 			return -ENOMEM; //TODO: add teardown code
 
 		priv->irqchip_data[i].domain = domain;
-		priv->irqchip_data[i].reg_mask = mpfs_irq_mux_masks[i - 38];
-		priv->irqchip_data[i].offset = (i - 38) * 32;
+		priv->irqchip_data[i].reg_mask = mpfs_irq_mux_masks[i - MPFS_MUX_NUM_DIRECT_IRQS];
+		priv->irqchip_data[i].offset = (i - MPFS_MUX_NUM_DIRECT_IRQS) * MPFS_IRQS_PER_GPIO;
 
 		irq_set_chained_handler_and_data(priv->irqchip_data[i].irq,
 						 mpfs_irq_mux_irq_handler,
 						 &priv->irqchip_data[i]);
 
-		pr_info("mpfs-irq-mux: registered domain %d:%u\n", i, priv->irqchip_data[i].irq);
+		pr_info("registered domain %d:%u\n", i, priv->irqchip_data[i].irq);
 	}
 
-	pr_info("mpfs-irq-mux: registered with mux config %x\n", priv->mux_config);
+	pr_info("registered with mux config %x\n", priv->mux_config);
 	return 0;
 }
 
