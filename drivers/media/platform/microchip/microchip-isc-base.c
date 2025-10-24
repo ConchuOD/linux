ConchuOD/linux

commit 164da738e594b99fdd1097adb35c32e949358ab0

media: microchip-isc: enable userspace histogram statistics export
Export raw histogram data to userspace only when all 4 Bayer channels
are ready and userspace has active listeners. Also continue AWB work as
long as userspace listeners are active.

Signed-off-by: Balakrishnan Sambath <balakrishnan.s@microchip.com>
Signed-off-by: Balamanikandan Gunasundar <balamanikandan.gunasundar@microchip.com>

drivers/media/platform/microchip/microchip-isc-base.c
@@ -1452,6 +1452,15 @@ static void isc_awb_work(struct work_struct *w)
 	if (hist_id != ISC_HIS_CFG_MODE_B) {
 		hist_id++;
 	} else {
+		/* All 4 channels processed - notify userspace */
+		if (isc_stats_active(&isc->stats))
+			isc_stats_isr(&isc->stats);
+		else
+			dev_info(isc->dev, "No active userspace listeners\n");
+	}
+
+	/* Continue with AWB processing only if AWB is enabled */
+	if (ctrls->awb != ISC_WB_NONE) {
 		isc_wb_update(ctrls);
 		hist_id = ISC_HIS_CFG_MODE_GR;
 	}
@@ -1501,7 +1510,7 @@ static void isc_awb_work(struct work_struct *w)
 	mutex_lock(&isc->awb_mutex);
 
 	/* streaming is not active anymore */
-	if (isc->stop) {
+	if (isc->stop && !isc_stats_active(&isc->stats)) {
 		mutex_unlock(&isc->awb_mutex);
 		return;
 	}

commit 770d4d5214353c93d46761b11bf0072ef7e98150

media: microchip-isc: fix histogram ID reset
Reset histogram ID to ISC_HIS_CFG_MODE_GR after processing all four
Bayer channels, independent of AWB state. Move the assignment outside
the AWB check to preserve correct histogram cycling.

Fixes: 164da738e594 ("media: microchip-isc: enable userspace histogram statistics export")

Reviewed-by: Balamanikandan Gunasundar <balamanikandan.gunasundar@microchip.com>
Signed-off-by: Balakrishnan Sambath <balakrishnan.s@microchip.com>

drivers/media/platform/microchip/microchip-isc-base.c
@@ -1457,11 +1457,11 @@ static void isc_awb_work(struct work_struct *w)
 			isc_stats_isr(&isc->stats);
 		else
 			dev_info(isc->dev, "No active userspace listeners\n");
-	}
 
-	/* Continue with AWB processing only if AWB is enabled */
-	if (ctrls->awb != ISC_WB_NONE) {
-		isc_wb_update(ctrls);
+		/* Continue with AWB processing only if AWB is enabled */
+		if (ctrls->awb != ISC_WB_NONE)
+			isc_wb_update(ctrls);
+
 		hist_id = ISC_HIS_CFG_MODE_GR;
 	}
 